93b6d263b1508556d63a979a705684618333a54d
vidya.shunmuga@impetus.com <vidya.shunmuga@impetus.com>
2023-04-05 08:09:27 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+--from raw_order
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordercostprice,
+o.ordersellingprice,
+--from raw_customer
+c.customername,
+c.segment,
+c.country,
+--from raw_product
+p.category,
+p.productname,
+p.subcategory,
+ordersellingprice - ordercostprice as orderprofit
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
